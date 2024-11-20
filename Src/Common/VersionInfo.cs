6f98f70b480f9a1211b4fc40b46bf2784b813b83
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-20 19:24:05 +0800

    init instance

---
+++ Src/Common/VersionInfo.cs
@@ -0,0 +1,12 @@
+namespace Binance.Common
+{
+    public static class VersionInfo
+    {
+        private static string version = "4.1.0";
+
+        public static string GetVersion
+        {
+            get { return version; }
+        }
+    }
+}
